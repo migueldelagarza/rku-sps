c7c173eb48131e8453fe800291a4617450515636
Miguel de la Garza <hola@migueldelagarza.dev>
2023-07-17 13:42:00 -0600

    Se agrega where para sólo mostrar usuarios activos #1

--- employees/EMPALLCON.sql
+++ employees/EMPALLCON.sql
@@ -1,9 +1,9 @@
 DELIMITER //
--- SP para consultar todos los empleados
+-- SP para consultar todos los empleados activos
 -- Se imite id por seguridad
-CREATE PROCEDURE EMPALLCON()
+CREATE OR REPLACE PROCEDURE EMPALLCON()
 BEGIN
-    SELECT name, account, role FROM employees;
+    SELECT name, account, role FROM employees WHERE isActive = 1;
 END //
 
 DELIMITER ;
