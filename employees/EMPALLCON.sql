afb5687a3fce0a05080cbac34ade54190a565243
Miguel de la Garza <hola@migueldelagarza.dev>
2023-07-18 10:25:04 -0600

    correccion en documentación

--- employees/EMPALLCON.sql
+++ employees/EMPALLCON.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 -- SP para consultar todos los empleados activos
--- Se imite id por seguridad
+-- Se omite id por seguridad
 CREATE OR REPLACE PROCEDURE EMPALLCON()
 BEGIN
     SELECT name, account, role FROM employees WHERE isActive = 1;
